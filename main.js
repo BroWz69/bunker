ad31b121b7d6f7e8f376c270c1555890be8da27a
BroWz69 <Email.Bryan.Secondaire@gmail.com>
2019-08-01 14:33:15 +0200

    optimisation = config = prefix = eslinter

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Je suis prêt !");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.reply('Pong!');
-  if (msg.content === '@everyone') msg.channel.send('@everyone, salut à tous !', { disableEveryone: false });
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), salut à tous !');
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
 });
 
-client.login('NjA2NDE2NTUwNTc2MjU5MDgy.XULBdw.DL1RAGDqbJOuPhRlwiN6pXQwIDU');
+client.login(TOKEN);
