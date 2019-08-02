827768edaf2b272403b04eeab577f79675164790
BroWz69 <Email.Bryan.Secondaire@gmail.com>
2019-08-02 11:18:36 +0200

    remove file config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  TOKEN: "NjA2NDE2NTUwNTc2MjU5MDgy.XUP8vw.aZpUzC74xo_BKH1ws2INdlJcT4E",
-  PREFIX: "!",
-  DEFAULTSETTINGS: {
-    prefix: "!",
-    welcomeChannel: "logs",
-    welcomeMessage: "Welcome {{user}} !"
-  }
-};
